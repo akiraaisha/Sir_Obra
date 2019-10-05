19b81ce61340ca6bd807366804d0fb0e91cf2119
Julian Lugod <julianlugod@yahoo.com>
2019-10-05 22:26:49 +0800

    Update Jershey_Grade_Vowels_Positive_ver2.cpp

--- Jershey_Grade_Vowels_Positive_ver2.cpp
+++ Jershey_Grade_Vowels_Positive_ver2.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-	int option;
+	int input_option;
 	cout << "PROGRAM MENU: " << endl << "1. GRADE COMPUTATION" << endl << "2. Vowels and Consonants" << endl << "3. Positive and Negative Numbers" << endl;
 	cout << "Select an option 1-3: ";
-	cin >> option;
+	cin >> input_option;
 
-	switch (option) {
+	switch (input_option) {
 	case 1:
 		int math, science, english, history, ict, ave_grade;
 		cout << endl << "Grade Computation" << endl;
@@ -22,7 +22,7 @@ int main() {
 		cin >> history;
 		cout << "Enter Grade for ICT: ";
 		cin >> ict;
-		ave_grade = (math + science + english + history + ict) / 5;
+		ave_grade = (math + science + english + history + ict) / 5;//Formula
 		cout << "\nAverage Grade is: " << ave_grade << endl;
 		if (ave_grade >= 98) {
 			cout << "Equivalent grade is: 1.00";
@@ -71,14 +71,14 @@ int main() {
 		break;
 
 	case 2: {
-		char letter_input[255];
+		char letter_input[255];//variable array with a maximum of 255 characters.
 		cout << endl << "Enter a Letter: ";
 		cin >> letter_input;
 
-		for (int i = 0; i < strlen(letter_input); i++) {
-			if (isdigit(letter_input[i])) {
+		for (int i = 0; i < strlen(letter_input); i++) {//looping for our array letter_input to scan every block if there's a letter. Check also the purpose of strlen() function.
+			if (isdigit(letter_input[i])) {//isdigit(variable) is a function for checking if the variable is a digit.
 				cout << letter_input << " is not a letter. Invalid input.";
-				break;
+				break;//this will break the loop. So it will loop only once. If you don't use BREAK, the program will be repeating outputs.
 			}
 
 			else if (letter_input[i] == 'A' || letter_input[i] == 'E' || letter_input[i] == 'I' || letter_input[i] == 'O' || letter_input[i] == 'U' ||
